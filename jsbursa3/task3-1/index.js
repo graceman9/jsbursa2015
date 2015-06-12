0abecb3a5f251903bfc556897d0e58ddc9678716
Sergey Klochko <graceman9@gmail.com>
2015-06-12 11:00:42 +0300

    task3-1 Move HTML creation from script to index.html

--- jsbursa3/task3-1/index.js
+++ jsbursa3/task3-1/index.js
@@ -8,10 +8,9 @@ function jsonpSuccessHandler(data) {
   'use strict';
   var main = function main() {
     var head = document.querySelector('head');
-    var body = document.querySelector('body');
-    var input = document.createElement('input');
-    var button = document.createElement('button');
-    var content = document.createElement('content');
+    var input = document.querySelector('input');
+    var button = document.querySelector('button');
+    var content = document.querySelector('#content');
     var script;
     var handlerClick = function handlerClick() {
       script = document.createElement('script');
@@ -19,12 +18,6 @@ function jsonpSuccessHandler(data) {
       script.src = 'http://en.wikipedia.org/w/api.php?action=parse&page='+ input.value +'&prop=text&section=0&format=json&callback=jsonpSuccessHandler';
       script.parentNode.removeChild(script);
     };
-    content.id = 'content';
-    button.textContent = 'Отправить';
-    body.appendChild(input);
-    body.appendChild(button);
-    body.appendChild(content);
-
     button.addEventListener('click', handlerClick);
   };
   window.addEventListener('load', main);
