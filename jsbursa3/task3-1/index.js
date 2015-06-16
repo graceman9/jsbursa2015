057596274f63f2bedacd67fdb03b68120e52aa51
Sergey Klochko <graceman9@gmail.com>
2015-06-16 17:59:35 +0300

    task3-1 fix lint errors

--- jsbursa3/task3-1/index.js
+++ jsbursa3/task3-1/index.js
@@ -1,24 +1,21 @@
-function jsonpSuccessHandler(data) {
-  var content = document.querySelector('#content');
-  console.log(data);
-  content.innerHTML = data.parse.text['*'];
-}
-
-(function appJSONP(){
+(function appJSONP() {
   'use strict';
   var main = function main() {
     var head = document.querySelector('head');
     var input = document.querySelector('input');
     var button = document.querySelector('button');
-    var content = document.querySelector('#content');
     var script;
     var handlerClick = function handlerClick() {
       script = document.createElement('script');
       head.appendChild(script);
-      script.src = 'http://en.wikipedia.org/w/api.php?action=parse&page='+ input.value +'&prop=text&section=0&format=json&callback=jsonpSuccessHandler';
+      script.src = 'http://en.wikipedia.org/w/api.php?action=parse&page=' + input.value + '&prop=text&section=0&format=json&callback=jsonpSuccessHandler';
       script.parentNode.removeChild(script);
     };
     button.addEventListener('click', handlerClick);
+  };
+  window.jsonpSuccessHandler = function jsonpSuccessHandler(data) {
+    var content = document.querySelector('#content');
+    content.innerHTML = data.parse.text['*'];
   };
   window.addEventListener('load', main);
 }());
